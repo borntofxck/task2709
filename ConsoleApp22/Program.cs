815fccf01b1e4056696a69b03a094af2e65f796a
kvtsuki <117033172+kvtsuki@users.noreply.github.com>
2023-09-27 22:37:46 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите четырехзначное число: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                if (number >= 1000 && number <= 9999)
+                {
+                    int digitSum = 0;
+                    int digitProduct = 1;
+                    int tempNumber = number;
+
+                    while (tempNumber != 0)
+                    {
+                        int digit = tempNumber % 10;
+                        digitSum += digit;
+                        digitProduct *= digit;
+                        tempNumber /= 10;
+                    }
+
+                    bool conditionResult = digitSum > digitProduct;
+                    Console.WriteLine(conditionResult ? "true" : "false");
+                }
+                else
+                {
+                    Console.WriteLine("Введено нечетырёхзначное число.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Введите целое число.");
+            }
+        }
+        
+        }
+    }
+
